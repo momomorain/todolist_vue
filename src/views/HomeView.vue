e13da8a7f850af2e2bb9b8e9549e0916f112e6ee
momomorain <160811973+momomorain@users.noreply.github.com>
2024-02-29 16:13:30 +0800

    B1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,6 @@ export default {
     return {
       addText: '',
       todoList: [
-        
 
       ],
       selectedTab: '',
@@ -25,10 +24,13 @@ export default {
           id: listID + 1,
           check: false,
           text: text,
+          editSwitch: false,
+          editMsg: '',
         }
         this.todoList.push(item);
         console.log(this.todoList);
       }
+      localStorage.setItem('msg', JSON.stringify(this.todoList))
     },
     getInputValue(e) {
       this.addText = e.target.value;
@@ -46,7 +48,23 @@ export default {
     },
     changTab(string) {
       this.selectedTab = string;
+    },
+    editMsg(item) {
+      console.log(item);
+      item.editSwitch = !item.editSwitch;
+      if (item.editMsg !== '') {
+        item.text = item.editMsg;
+      }
+      if (item.editSwitch === true) {
+        todoList.editMsg = item.editMsg;
+      }
     }
+  },
+  mounted() {
+    // 當網頁開啟後，先執行裡面的JS
+    // 先把資料從localStorage 特定Key拿出資料來，丟入msgArr裡面
+    console.log(123);
+
   },
   computed: {
     fileterData() {
@@ -63,7 +81,7 @@ export default {
   <div class="w-screen h-screen flex justify-center items-center bg-gray-800 ">
     <div class="w-3/4 h-3/4 flex-col justify-center items-center p-5 bg-gradient-to-l from-gray-500 to-zinc-800">
       <div class="p-5 flex items-center">
-        <input type="text" class="w-[250px] mr-2 text-black" @change="(e) => getInputValue(e)">
+        <input v-model="addText" type="text" class="w-[250px] mr-2 text-black">
         <button class="button bg-zinc-600" @click="addItemList">新增</button>
       </div>
       <div class="flex gap-4">
@@ -80,11 +98,17 @@ export default {
         </tr>
         <tr v-for="(item, index) in todoList" :key="index">
           <td class="text-white text-center mt-3">
-            <input type="checkbox" class="ml-18">
+            <input v-model="todoList.check" type="checkbox" class="ml-18">
           </td>
-          <td class="text-white text-center ">{{ item.text }}</td>
+          <td v-if="item.editSwitch === false" class="text-white text-center ">{{ item.text }}</td>
+          <td v-else class="text-black text-center ">
+            <input v-model="item.editMsg" type="text">
+          </td>
+
+
           <td class="text-white text-center">
-            <button class="button bg-zinc-500" @click="edit(index)">編輯</button>
+            <!-- <button class="button bg-zinc-500" @click="edit(index)">編輯</button> -->
+            <button class="button bg-zinc-500" @click="editMsg(item)">編輯</button>
             <button class="button bg-zinc-500 ml-2" @click="deleter(index)">刪除</button>
           </td>
         </tr>
